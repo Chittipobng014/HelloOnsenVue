10621567759e815e0b4f4d0022093c4fd39f49f7
Chittiphong <tpl_army@hotmail.com>
2018-07-18 18:45:56 +0700

    commitmitmitmit

---
+++ src/components/DeviceList.vue
@@ -0,0 +1,23 @@
+<template>
+        <v-ons-list>
+            <v-ons-list-header>Nearby Devices</v-ons-list-header>
+                <v-ons-list-item modifier="chevron" v-for="device in devices" :key="device.device_id">
+                    <div class="left">{{device.displayName}}</div>
+                    <div class="right"><v-ons-button @click="connectTo(device.device_id)">Connect</v-ons-button></div>
+                </v-ons-list-item>
+        </v-ons-list>
+</template>
+
+<script>
+export default {
+    name: 'deviceList',
+    data(){
+        return{
+            devices: []
+        }
+    },
+    methods:{
+        
+    }
+}
+</script>
---
+++ src/components/ScanButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-ons-button >Scan</v-ons-button>
+</template>
+
+<script>
+export default {
+    name:'scanButton',
+    data(){
+        var data = []
+        return{
+            devices: data
+        }
+    },
+    methods:{
+       deviceData:function(){
+           console.log(this.devices)
+           ble.startScan([], function (res) {
+                console.log(JSON.stringify(res));
+                var device = [];
+                var displayName;
+                if (res.name) {
+                    displayName = res.name;
+                }
+                else {
+                    displayName = res.id;
+                }
+                this.devices.push({displayName: displayName, device_id: res.id});
+                console.log(JSON.stringify(this.$data.devices));
+                }, function (error) {
+                console.log(JSON.stringify(error));
+                });
+       },
+    }
+}
+</script>
