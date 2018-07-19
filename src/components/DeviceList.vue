6bc02327ee3fbaa1e2296a2e22b7b27556a9f50d
Chittiphong <tpl_army@hotmail.com>
2018-07-19 16:31:11 +0700

    connect and disconnect

--- src/components/DeviceList.vue
+++ src/components/DeviceList.vue
@@ -1,43 +1,21 @@
-<template id="main">
-  <v-ons-navigator swipeable
-    :page-stack="pageStack"
-    @push-page="pageStack.push($event)"
-  ></v-ons-navigator>
-</template>
-
-<template id="page1">       
-    <v-ons-list-item modifier="chevron">
-        <div class="left">Name: {{name}}</div>
-        <div class="right" @click="push"><v-ons-button @click="connectTo(id)">{{state}}</v-ons-button></div>
+<template>       
+    <v-ons-list-item>
+        <div class="left">{{name}}</div>
+        <div class="right"><v-ons-button @click="connectTo(id)">{{state}}</v-ons-button></div>
     </v-ons-list-item>        
 </template>
 
-<template id="page2">
-    
-</template>
-
-
 <script>
-const page1 = {
-    key: 'page1',
-    template: '#page1',
-    props: {
-        name:{
-            type: String,
-            required: true 
-        },
-        id:{
-            type: String,
-            required: true 
-        }
-    },
+
+export default {    
+    name: "deviceList",
+    props: ["name", "id"],
     data(){
         return{
-            state: 'Connect',
-            device_id: this.id
+            state: 'Connect'
         }
     },
-    methods: {
+    methods:{
         connectTo: function(id){
             ble.isConnected(id, (is) =>{
                     ble.disconnect(id, (success) =>{
@@ -54,43 +32,6 @@ const page1 = {
                         console.log(JSON.stringify(error));              
                     };
                 });
-        },
-        push:() => {
-            this.$emit('push-page', {
-                page2,
-                onsNavigatorProps:{
-                    id: this.device_id
-                }
-            });
-        }
-    }
-}
-
-const page2 = {
-    key: 'page2',
-    template: '#page2',
-    props: {
-        id: {
-            type: String,
-            required: true
-        }
-    }
-}
-
-export default {
-    
-    name: "deviceList",
-    template: '#main',
-    props: ["name", "id"],
-    data(){
-        return{
-            pageStack: [{
-                            page1,
-                            onsNavigatorProps:{
-                                id: this.id,
-                                name: this.name
-                            } 
-                        }]
         }
     }
 };
